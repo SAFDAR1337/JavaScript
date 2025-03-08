70be96ef10dbed1b80775329038f51a0b8c5920d
SAFDAR1337 <118106718+SAFDAR1337@users.noreply.github.com>
2025-03-08 12:38:02 +0000

    check arry is object

---
+++ Arrays/1.js
@@ -0,0 +1,14 @@
+let myarry = [123,"safdar",{1:"string"}];
+// console.log(myarry[0]);
+// console.log(typeof(myarry[2]));
+myarry[0]=["stringsafdar",123];
+// console.log(myarry[0]);
+let myString="safdar";
+// console.log(myString);
+let makingNewArryFromStr=myString.split("").join(",");
+console.log(makingNewArryFromStr);
+console.log(typeof makingNewArryFromStr);
+let myarr1=[10,20,30];
+console.log(typeof myarr1);
+console.log(myarr1 instanceof Array)
+console.log(myarr1 instanceof Object)
