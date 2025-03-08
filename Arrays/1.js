924e0722798eaf1a956098dfe0d6e2f7b12e2ffc
SAFDAR1337 <118106718+SAFDAR1337@users.noreply.github.com>
2025-03-08 12:45:22 +0000

    array lenght property modifyable

--- Arrays/1.js
+++ Arrays/1.js
@@ -1,14 +1,20 @@
-let myarry = [123,"safdar",{1:"string"}];
-// console.log(myarry[0]);
-// console.log(typeof(myarry[2]));
-myarry[0]=["stringsafdar",123];
-// console.log(myarry[0]);
-let myString="safdar";
-// console.log(myString);
-let makingNewArryFromStr=myString.split("").join(",");
-console.log(makingNewArryFromStr);
-console.log(typeof makingNewArryFromStr);
-let myarr1=[10,20,30];
-console.log(typeof myarr1);
-console.log(myarr1 instanceof Array)
-console.log(myarr1 instanceof Object)
+// let myarry = [123,"safdar",{1:"string"}];
+// // console.log(myarry[0]);
+// // console.log(typeof(myarry[2]));
+// myarry[0]=["stringsafdar",123];
+// // console.log(myarry[0]);
+// let myString="safdar";
+// // console.log(myString);
+// let makingNewArryFromStr=myString.split("").join(",");
+// console.log(makingNewArryFromStr);
+// console.log(typeof makingNewArryFromStr);
+// let myarr1=[10,20,30];
+// console.log(typeof myarr1);
+// console.log(myarr1 instanceof Array)
+// console.log(myarr1 instanceof Object)
+// let make new arry to test length property 
+const myarr1 = ["Apple","Banana","Mango"];
+// console.log(myarr1)
+// console.log(myarr1.length)
+// myarr1.length=2;
+// console.log(myarr1);  Array lenght property can be modified // 
